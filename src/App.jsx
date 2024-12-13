03586c6bb73f0a6861de64536de13365b8e2911d
Benj4865 <bena380dk@gmail.com>
2024-12-13 10:59:18 +0100

    Change name and path to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,4 @@
-/*import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
-*/
 // Git Test
 // Import necessary dependencies
 // eslint-disable-next-line no-unused-vars
@@ -66,14 +31,14 @@ const App = () => {
             const data = await response.json();
             setResponseMessage(JSON.stringify(data)); // Update state with response message
         } catch (error) {
-            console.error('Error communicating with backend:', error);
-            setResponseMessage('Error communicating with backend.');
+            console.error('Error communicating with the backend or database:', error);
+            setResponseMessage('Error communicating with the backend or database.');
         }
     };
 
     return (
         <div style={{ padding: '20px', fontFamily: 'Arial' }}>
-            <h1>React-C# Backend Example</h1>
+            <h1>Overview of Person</h1>
             <input
                 type="text"
                 placeholder="Enter User ID"
